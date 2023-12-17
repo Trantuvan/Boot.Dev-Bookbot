d64719cc26d9d43ac8bbffaee1eab131fce39cb5
trantuvan <trantuvan.kan@gmail.com>
2023-12-17 22:46:31 +0700

    count each words in document

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    num_words = count_words(text)
+    # print(text)
+    print(f"{num_words} words found in the document")
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def count_words(book_str):
+    return len(book_str.split())
 
 main()
