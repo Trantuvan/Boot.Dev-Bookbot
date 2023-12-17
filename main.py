8afbeb5a4f106f2b587308be02c0e1d2eaca6efc
trantuvan <trantuvan.kan@gmail.com>
2023-12-17 23:05:13 +0700

    count apperance of each letter in document

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = count_words(text)
+    num_letters = count_letter(text)
     # print(text)
     print(f"{num_words} words found in the document")
+    print(f"{num_letters} letters found in the document")
 
 def get_book_text(path):
     with open(path) as f:
@@ -11,5 +13,17 @@ def get_book_text(path):
 
 def count_words(book_str):
     return len(book_str.split())
+
+def count_letter(book_str):
+    normalized_str = book_str.lower()
+    letters = {}
+
+    for str in normalized_str:
+        if str in letters:
+            letters[str] += 1
+            continue
+        letters[str] = 1
+
+    return letters
 
 main()
